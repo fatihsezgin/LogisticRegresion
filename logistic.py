1e6c506d4e57a778f5cb4b076683793ebce08027
fatihsezgin <fatihsezgin98@hotmail.com>
2020-04-03 21:58:21 +0300

    comments for the functions has been added

--- logistic.py
+++ logistic.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import numpy as np
-from numpy import loadtxt, where
+import matplotlib.pyplot as plt
+from numpy import where
 from pylab import scatter, show, legend, xlabel, ylabel
+from decimal import Decimal
 
+#columns that is going to readed into csv
 columns = ["SepalLengthCm", "SepalWidthCm", "PetalLengthCm", "PetalWidthCm"]
 
 data = pd.read_csv('iris.csv', usecols=[1, 2, 3, 4, 5])
@@ -11,7 +14,6 @@ data = pd.read_csv('iris.csv', usecols=[1, 2, 3, 4, 5])
 a = data[columns]
 a = np.array(a)
 X = []
-
 # To append 1 to the each element in the a
 for i in range(len(a)):
     tmp = np.array([1])
@@ -19,26 +21,15 @@ for i in range(len(a)):
     X.append(tmpArray)
 
 X = np.array(X)
-# print(X)
 
-Y = data[["Species"]]
-Y = np.array(Y)
-for i in range(len(Y)):
-    if Y[i] == "Iris-setosa":
-        Y[i] = 1
-    else:
-        Y[i] = 0
-'''
-pos = where(Y == 1)
-neg = where(Y == 0)
-scatter(X[pos, 0], X[pos, 1], marker='o', c='b')
-scatter(X[neg, 0], X[neg, 1], marker='x', c='r')
-legend(['Iris-Setosa', 'Iris-Verticolor'])
-show()
+target_map = {'Iris-setosa':0,
+              'Iris-versicolor':1}
 
-'''
+data[["Species"]] = data['Species'].apply(lambda x: target_map[x])
+Y = np.array(data[["Species"]])
 
-testData = pd.read_csv('iristest.csv', usecols=[1, 2, 3, 4, 5])
+
+testData = pd.read_csv('iristest2.csv', usecols=[1, 2, 3, 4, 5])
 b = testData[columns]
 b = np.array(b)
 X_test = []
@@ -51,49 +42,51 @@ for i in range(len(b)):
 
 X_test = np.array(X_test)
 
-# print(X_test)
-Y_test = testData[["Species"]]
-Y_test = np.array(Y_test)
-for i in range(len(Y_test)):
-    if Y_test[i] == "Iris-setosa":
-        Y_test[i] = 1
-    else:
-        Y_test[i] = 0
-
-# print(Y_test)
-
-theta = [0, 0, 0, 0, 0]
-iterations = 1000
-alpha = 0.1
+testData[["Species"]] = testData['Species'].apply(lambda x: target_map[x])
+Y_test = np.array(testData[["Species"]])
 
 
+ 
+theta = [0, 0, 0, 0, 0] # declare the initial theta
+iterations = 1000 # iteration
+alpha = 0.1 #learning rate
+
+
+# @param z : applies sigmoid function to given parameter
+# returns : a number between 0-1
 def Sigmoid(z):
     return float(1.0 / float((1.0 + np.exp(-1.0 * z))))
 
-
+# @param coefficients @type ndarray 
+# @returns value that applied sigmoid function in given feature vector.
 def Hypothesis(coefficients):
     z = 0
     for i in range(len(theta)):
         z += coefficients[i] * theta[i]
     return Sigmoid(z.astype(np.float128))
 
-
-def CostFunction(prediction):
+# cost function calculated for n observations which is length of Y
+# function {i=1:n}Σ -yilog(h(xi))- (1-yi)log(1-h(xi))
+def CostFunction():
     sumOfErrors = 0
-    # cost function calculated for n observations which is length of Y
     for i in range(len(Y)):
         if Y[i] == 1:
-            error = Y[i] * np.log(prediction)
+            error = Y[i] * np.log(Hypothesis(X[i]))
         elif Y[i] == 0:
-            error = (1 - Y[i]) * np.log(1 - prediction)
+            error = (1 - Y[i]) * np.log(1 - Hypothesis(X[i]))
         sumOfErrors += error
     return (-1 / len(Y)) * sumOfErrors
 
 
-# @param j : theta index
+# @param j : theta index that will be calculated
+# @returns the new value for theta[j]
+# function βj = βj- a{i=1:n}Σ(h(xi)-yi)xij
+
+# where β is the theta array , a = learning rate,  xi = feature vector, xij = β value for xi
+# yi = label of the feature vector
 def CostDerivative(j):
     sumErrors = 0
-    n = len(Y)
+    n = len(Y)  
     for i in range(n):
         xi = X[i]
         xij = xi[j]
@@ -104,39 +97,92 @@ def CostDerivative(j):
     return theta[j]
 
 
+# With GradientDescent, iterates all of the theta values, calls @func CostDerivative for each element in theta
+# updates the new value.
 def GradientDescent():
     new_theta = []
     for i in range(len(theta)):
         new_theta.append(CostDerivative(i))
     return new_theta
+    
+# calculates the regression coefficients in given feature vectors.
+def estimate_coef(x,y):
+    # number of observations/points 
+    n = np.size(x) 
+  
+    # mean of x and y vector 
+    m_x, m_y = np.mean(x), np.mean(y) 
+  
+    # calculating cross-deviation and deviation about x 
+    SS_xy = np.sum(y*x) - n*m_y*m_x 
+    SS_xx = np.sum(x*x) - n*m_x*m_x 
+  
+    # calculating regression coefficients 
+    b_1 = SS_xy / SS_xx 
+    b_0 = m_y - b_1*m_x 
+  
+    return(b_0, b_1)
+    
+# plots the regression line with given feature vectors x and y     
+def plot_regression_line(x, y, b): 
+    versicolor = np.where(Y == 1)
+    setosa = np.where(Y == 0)
+
+    plt.figure()
+    # plotting the actual points as scatter plot 
+    plt.scatter(X[versicolor[0], 3], X[versicolor[0], 4], color= 'r', label="Versicolor") # versicolor
+    plt.scatter(X[setosa[0], 3], X[setosa[0], 4], color = 'b',label="Setosa") # setosa
+    
+    # predicted response vector 
+    y_pred = b[0] + b[1]*x 
+  
+    # plotting the regression line 
+    plt.plot(x, y_pred, color = "k", label= "Regression Line") 
+  
+    # putting labels 
+    plt.xlabel('Petal Length')
+    plt.ylabel('Petal Width')
+    plt.legend()
+    # function to show plot 
+    plt.show() 
 
 
+# Predicts a number with trained theta coefficients
 def Predict():
     score = 0
     length = len(X_test)
     for i in range(length):
-        # floatPrediction = Hypothesis(X_test[i])
-        # print("Float prediction ", floatPrediction)
-        prediction = round(Hypothesis(X_test[i]))
+        prediction = round(Hypothesis(X_test[i]),5)
         answer = Y_test[i]
         print("prediction was " + str(prediction) + " answer was " + str(answer))
         if prediction == answer:
             score += 1
 
-    accuracy = ((score / len(X_test)) * 100)
-    print("score is = ", score)
-    print("accuracy is = ", str(accuracy) + " %")
+    accuracy = ((score / length) * 100)
+    print("score is = " +str(score) + " out of " + str(length) + " samples")
+    print('accuracy is =  {} %.'.format(accuracy))
 
 
-def LogisticRegression(X, Y):
+def LogisticRegression():
     for i in range(iterations):
         new_theta = GradientDescent()
         theta = new_theta
         if i % 100 == 0:
-            print("theta: ", theta)
+            print('Cost function : {:.10f}'.format(CostFunction()[0]))
+    print("theta: ", theta)
     Predict()
-    # prediction = Hypothesis(X[i])
-    # print(CostFunction(prediction))
+
+    #used for estimating the regression lines between the features.
+    # b = estimate_coef(X[:,1], X[:,2])
+    # plot_regression_line(X[:,1], X[:,2], b)
+    # b = estimate_coef(X[:,1], X[:,3])
+    # plot_regression_line(X[:,1], X[:,3], b)
+    # b = estimate_coef(X[:,1], X[:,4])
+    # plot_regression_line(X[:,1], X[:,4], b)
+    # b = estimate_coef(X[:,2], X[:,3])
+    # plot_regression_line(X[:,2], X[:,3], b)
+    # b = estimate_coef(X[:,2], X[:,4])
+    # plot_regression_line(X[:,2], X[:,4], b)
 
 
-LogisticRegression(X, Y)
+LogisticRegression()
